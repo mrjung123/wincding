081af3fdcadc49e316cbd219a04d28c802366402
mrjung123 <martin@jungfotografie.nl>
2021-07-04 17:05:56 +0200

    Add files via upload

---
+++ Winc_Academy/opdracht_11/script.js
@@ -0,0 +1,16 @@
+/*
+console.log("Hallo Winc Academy studenten");
+// Dit is een grote som
+let getala = 1230941;
+let getalb = 1823794;
+let uitkomst = getala*getalb;
+console.log("Uitkomst van ",getala,"x",getalb, "=",uitkomst);
+// Dit is een nieuwe som
+getala = 637263;
+getalb = 54;
+uitkomst = getala/getalb;
+console.log("Uitkomst van ",getala,"/",getalb, "=",uitkomst);
+// uitkomsten op het scherm!!
+*/
+
+//Door de block comment is er niets oop het console zichtbaar!!
