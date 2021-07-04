0664fe7338e195ce83fdca940611f738b1c7924e
mrjung123 <martin@jungfotografie.nl>
2021-07-04 12:23:59 +0200

    Add files via upload

---
+++ Winc_Academy/opdracht_10/script.js
@@ -0,0 +1,21 @@
+console.log("Hello Winc Academy");
+let naam = "Mijn naam"
+console.log(naam);
+
+//  if is een gereserveerd woord en daarom niet te grbuiken als variabelen!!
+
+let uitkomsta = 4 + 4;
+let uitkomstb = "4" + "4";
+// string worden "aan elkaar geplakt"
+
+console.log(uitkomsta);
+console.log(uitkomstb);
+
+console.log(18+2);
+console.log(18-2);
+console.log(18*2);
+console.log(18/2);
+console.log(5%2);
+
+//typeof 48;  // getal
+//typeof "48";  // text
