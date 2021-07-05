dd0707157c567f9d94b39533712470326c237095
mrjung123 <martin@jungfotografie.nl>
2021-07-05 13:06:33 +0200

    Add files via upload

---
+++ Winc_Academy/opdracht_16/script.js
@@ -0,0 +1,23 @@
+const verf = function(){
+    console.log("The wall has been painted red");
+}
+verf();
+
+
+const verven = function(kleur){
+    console.log("The wall has been painted " + kleur);
+}
+
+verven("green");
+verven("yellow");
+verven();
+
+
+
+const watverven = function(wat, kleur){
+    console.log("The " + wat + " wall has been painted " + kleur);
+}
+
+watverven("binnen", "rood");
+
+
