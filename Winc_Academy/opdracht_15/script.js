8bcd9d97ee69b89b9b37767c4f2bacc96c7bde2f
mrjung123 <martin@jungfotografie.nl>
2021-07-05 12:38:47 +0200

    Add files via upload

---
+++ Winc_Academy/opdracht_15/script.js
@@ -0,0 +1,26 @@
+//  functies
+
+const pizzamaken = function(){
+    console.log("PIZZA MAKE DOE JE ZO:");
+    console.log("Maak deeg");
+    console.log("voeg ingredienten toe");
+    console.log("stop de pizza in de oven");
+    console.log("___________________________");
+}
+
+pizzamaken();
+
+const sushimaken = function(){
+    console.log("Sushi maken doe je zo:");
+    console.log("Rijst koken");
+    console.log("Leg alles op het zeewier");
+    console.log("Oprollen en snijden");
+    console.log("___________________________");
+}
+
+
+sushimaken();
+sushimaken();
+sushimaken();
+pizzamaken();
+pizzamaken();
